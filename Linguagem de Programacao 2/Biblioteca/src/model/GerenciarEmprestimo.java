08fa91e0943e44a1e478dfdca0bbe6fff20ae8e5
Lucas Oliveira <lucassdeoliveira@outlook.com.br>
2024-02-18 16:21:26 -0300

    Devolução implementada + refinamento do emprestimo

--- Linguagem de Programacao 2/Biblioteca/src/model/GerenciarEmprestimo.java
+++ Linguagem de Programacao 2/Biblioteca/src/model/GerenciarEmprestimo.java
@@ -5,18 +5,32 @@ import java.util.ArrayList;
 public class GerenciarEmprestimo {
     private ArrayList<Emprestimo> emprestimosRealizados = new ArrayList<>();
     public void realizarEmprestimo(Emprestimo emprestimo) {
-        if (emprestimo.getAluno().getCotas() != 0) {
-            if (emprestimo.getLivro().getQtdLivros() != 0) {
-                emprestimosRealizados.add(emprestimo);
-                emprestimo.getAluno().setCotas(emprestimo.getAluno().getCotas() - 1);
-                emprestimo.getLivro().setQtdLivros(
-                        emprestimo.getLivro().getQtdLivros() - 1
-                );
-            }
+        if (!(emprestimo.getAluno().getCotas() != 0)) {
+            System.out.println("Aluno " + emprestimo.getAluno().getNome() + " não possui cotas suficientes!");
+        }else if (!(emprestimo.getLivro().getQtdLivros() != 0)) {
+            System.out.println("Livro " + emprestimo.getLivro().getTitulo() + " não possuí " +
+                    "mais nenhum exemplar no acervo");
+        } else {
+            emprestimosRealizados.add(emprestimo);
+            emprestimo.getAluno().setCotas(emprestimo.getAluno().getCotas() - 1);
+            emprestimo.getLivro().setQtdLivros(
+                    emprestimo.getLivro().getQtdLivros() - 1
+            );
+            System.out.println("Emprestimo realizado!\n" +
+                                " Nome: " + emprestimo.getAluno().getNome() + "\n"
+                                + " Código: " + emprestimo.getLivro().getTitulo()
+            );
         }
     }
 
     //Criar função para devolução de livro e incremento da qtd livros e qtd cotas do aluno
+    public void devolverEmprestimo(Emprestimo emprestimo) {
+        if (emprestimosRealizados.contains(emprestimo)) {
+            emprestimosRealizados.remove(emprestimo);
+            emprestimo.getAluno().setCotas(emprestimo.getAluno().getCotas() + 1);
+            emprestimo.getLivro().setQtdLivros(emprestimo.getLivro().getQtdLivros() - 1);
+        }
+    }
     //Criar função para adicionar uma lista de livros para um aluno apenas
 
     @Override
@@ -25,7 +39,7 @@ public class GerenciarEmprestimo {
         retorno.append("[ " + "\n");
         for (int i = 0; i < emprestimosRealizados.size(); i++) {
             Emprestimo emprestimo = emprestimosRealizados.get(i);
-            retorno.append(i + " - Aluno: " + emprestimo.getAluno().getNome() + " | Cod Matricula: " + emprestimo.getAluno().getCodigo() +
+            retorno.append(i + " - Aluno: " + emprestimo.getAluno().getNome() + " | Codigo: " + emprestimo.getAluno().getCodigo() +
                     " | Livro: " + emprestimo.getLivro().getTitulo() + " | Autora: " + emprestimo.getLivro().getNomeAutora() + "\n"
             );
         }
